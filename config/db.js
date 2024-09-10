2d2d7d35d8a0a88b0fc80dad442b6420be065a1a
Qianqian <qianqian_luo95@163.com>
2024-09-10 09:38:45 +0800

    fix: dotenv导入位置问题导致mysql配置文件读取.env失败 errorcode: -4078

--- config/db.js
+++ config/db.js
@@ -1,8 +1,12 @@
-const mysql = require('mysql2/promise')
+import dotenv from 'dotenv';
+dotenv.config();
+import mysql from 'mysql2/promise';
 
 // create the connection to database
 const pool = mysql.createPool({
-    host: process.env.MYSQL_HOST,
+    // host: process.env.MYSQL_HOST,
+    host: process.env.MYSQL_HOST,  // IP 地址
+    port: process.env.MYSQL_PORT,    
     user: process.env.MYSQL_USER,
     password: process.env.MYSQL_PWD,
     database: process.env.MYSQL_DB,
@@ -23,4 +27,4 @@ pool.getConnection()
   });
 
   
-  module.exports = pool
+export default pool;
